81f121c0122a881cc47dc8b2963bb1332b94a551
Alex Prokudin <prokudin89@gmail.com>
2021-12-29 14:07:47 +0700

    input cube faces

--- main.py
+++ main.py
@@ -21,8 +21,26 @@ class Color(Enum):
     Green = 5
     Blue = 6
 
+    @classmethod
+    def from_char(cls, char):
+        return Color.letters_map()[char]
+
+    @classmethod
+    def letters(cls):
+        return set(map(lambda c: c.get_color_letter(), cls))
+
+    @classmethod
+    def letters_map(cls):
+        result = {}
+        for c in Color:
+            result[c.get_color_letter()] = c
+        return result
+
     def __str__(self):
         return self.name
+
+    def get_color_letter(self):
+        return self.name[0].lower()
 
 
 class Face:
@@ -30,7 +48,12 @@ class Face:
 
     def __init__(self, side, colors):
         self.side = side
-        self.colors = colors
+        self.colors = []
+        if isinstance(colors, str):
+            for c in colors:
+                self.colors.append(Color.from_char(c))
+        else:
+            self.colors = colors
 
     def __str__(self):
         return "{0}, colors: {1}".format(self.side, ','.join(map(str, self.colors)))
@@ -39,18 +62,22 @@ class Face:
 class Cube:
     """Simple data model of Rubik's Cube"""
 
-    def __init__(self):
-        front = Face(Side.Front, [Color.White, Color.Blue, Color.Orange])
-        right = Face(Side.Right, [])
-        back = Face(Side.Back, [])
-        left = Face(Side.Left, [])
-        up = Face(Side.Up, [])
-        bottom = Face(Side.Bottom, [])
-        self.faces = [front, right, back, left, up, bottom]
+    def __init__(self, faces):
+        self.faces = faces
 
     def __str__(self):
         return '\n'.join(map(str, cube.faces))
 
 
-cube = Cube()
+# input cube faces
+print("Input colors could be one of: " + str(Color.letters()))
+faces = []
+for s in Side:
+    # TODO: check input values, string size and color letters
+    colorsRaw = input("Input colors of {0} face ".format(s))
+    face = Face(s, colorsRaw[:9])
+    print(face)
+    faces.append(face)
+
+cube = Cube(faces)
 print(str(cube))
