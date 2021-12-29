ba2c851053ddc8bafd9d820e2c3002a10015bc94
Alex Prokudin <prokudin89@gmail.com>
2021-12-29 16:15:16 +0700

    change Cubes print message

--- main.py
+++ main.py
@@ -49,6 +49,7 @@ class Face:
     def __init__(self, side, colors):
         self.side = side
         self.colors = []
+        self.size = 3
         if isinstance(colors, str):
             for c in colors:
                 self.colors.append(Color.from_char(c))
@@ -56,8 +57,16 @@ class Face:
             self.colors = colors
 
     def __str__(self):
-        return "{0}, colors: {1}".format(self.side, ','.join(map(str, self.colors)))
+        return "---{0}---\n{1}\n{2}\n{3}"\
+            .format(self.side,
+                    '\t'.join(map(str, map(lambda c: c.get_color_letter(), self.get_row(0)))),
+                    '\t'.join(map(str, map(lambda c: c.get_color_letter(), self.get_row(1)))),
+                    '\t'.join(map(str, map(lambda c: c.get_color_letter(), self.get_row(2)))))
 
+
+    def get_row(self, i):
+        # TODO: check input indexes
+        return [self.colors[i * self.size], self.colors[i * self.size + 1], self.colors[i * self.size + 2]]
 
 class Cube:
     """Simple data model of Rubik's Cube"""
@@ -71,13 +80,21 @@ class Cube:
 
 # input cube faces
 print("Input colors could be one of: " + str(Color.letters()))
-faces = []
-for s in Side:
-    # TODO: check input values, string size and color letters
-    colorsRaw = input("Input colors of {0} face ".format(s))
-    face = Face(s, colorsRaw[:9])
-    print(face)
-    faces.append(face)
+# faces = []
+# for s in Side:
+#     # TODO: check input values, string size and color letters
+#     colorsRaw = input("Input colors of {0} face ".format(s))
+#     face = Face(s, colorsRaw[:9])
+#     print(face)
+#     faces.append(face)
 
+faces = [
+    Face(Side.Front, "rbryrowyb"),
+    Face(Side.Right, "ggowggrbw"),
+    Face(Side.Back, "ygwwogoyb"),
+    Face(Side.Left, "grbrbrobo"),
+    Face(Side.Up, "rogwyyyry"),
+    Face(Side.Bottom, "gbwowwyob")
+]
 cube = Cube(faces)
-print(str(cube))
+print("\n\nYour Cube:\n" + str(cube))
